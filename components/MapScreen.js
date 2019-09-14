44d94cb044448fd577f27d21fa71d4df1b527b2c
Welvin <wewe2712@connect.hku.hk>
2019-09-14 15:41:39 -0400

    Add pins on the map

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,14 +1,46 @@
 import React,{ Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE,Marker } from 'react-native-maps';
 
 export default class MapScreen extends Component{
-    
+    constructor(props){
+      super(props);
+      this.state = {
+        markers : [
+          {
+            key : 1,
+            latlng : {
+              latitude: 43.470630,
+              longitude: -80.541380,
+            },
+            title : 'Vending machine 1',
+            description : 'price : \n quantity : '
+          },
+          {
+            key : 2,
+            latlng : {
+              latitude: 43.471630,
+              longitude: -80.541380,
+            },
+            title : 'Vending machine 2',
+            description : 'price : \n quantity : '
+          },
+          {
+            key : 3,
+            latlng : {
+              latitude: 43.469625,
+              longitude: -80.541380,
+            },
+            title : 'Vending machine 3',
+            description : 'price : \n quantity : '
+          }
+        ]
+      }
+    }
     render(){
         return(
           <View style={styles.container}>
             <MapView
-              provider={PROVIDER_GOOGLE} // remove if not using Google Maps
               style={styles.map}
               region={{
                 latitude: 43.470630,
@@ -17,6 +49,14 @@ export default class MapScreen extends Component{
                 longitudeDelta: 0.0121,
               }}
             >
+              {this.state.markers.map(marker => (
+                <Marker
+                  key={marker.key}
+                  coordinate={marker.latlng}
+                  title={marker.title}
+                  description={marker.description}
+                />
+              ))}
             </MapView>
           </View>
         );
@@ -26,7 +66,7 @@ export default class MapScreen extends Component{
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
+    height: '100%',
     width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
