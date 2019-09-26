33fc5f002c6163ce1cf833aaa51a5d8abdef799f
Welvin <wewe2712@connect.hku.hk>
2019-09-26 22:26:50 +0800

    Remove sensitive information

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 import Firebase from 'firebase';
 const firebaseConfig = {
-    
+    apiKey: "your_api_key",
+    authDomain: "hack-the-north-f883a.firebaseapp.com",
+    databaseURL: "https://hack-the-north-f883a.firebaseio.com",
+    projectId: "hack-the-north-f883a",
+    storageBucket: "hack-the-north-f883a.appspot.com",
+    messagingSenderId: "620410370510",
+    appId: "1:620410370510:web:0c56c54bb514d306cd139f"
   };
 let app = Firebase.initializeApp(firebaseConfig);
 export const db = app.database();
